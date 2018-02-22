0a05305e63b074d9886e60f968a96998dd981bfe
Đỗ Xuân Tiến <doxuantien@gmail.com>
2018-02-22 16:27:41 +0700

    Exercise 11 done

---
+++ Ex11AskingQuestion.py
@@ -0,0 +1,10 @@
+print("How old are you?",)
+age = input("How old are you?")
+print("How tall are you?",)
+height = input()
+print("How much do you weight?",)
+weight = input()
+
+print("So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight
+))
