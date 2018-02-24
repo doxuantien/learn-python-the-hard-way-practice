e7d86da1c2e7edb8f181358f5fb85d2395130fa0
Đỗ Xuân Tiến <doxuantien@gmail.com>
2018-02-24 16:04:14 +0700

    Exercise 25 done

---
+++ Ex25PracticeExcute.py
@@ -0,0 +1,18 @@
+import Ex25Practice
+
+sentence = "All good things come to those who wait"
+
+words = Ex25Practice.break_words(sentence)
+
+sorted_words = Ex25Practice.sort_words(words)
+
+Ex25Practice.print_first_word(words)
+Ex25Practice.print_last_word(words)
+
+Ex25Practice.print_first_word(sorted_words)
+Ex25Practice.print_last_word(sorted_words)
+
+sorted_words = Ex25Practice.sort_sentence(sentence)
+
+Ex25Practice.print_first_and_last(sentence)
+Ex25Practice.print_first_and_last_sorted(sentence)
